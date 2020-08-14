4f7025df1f6565097d09b8648694cdd62b3c4a10
Michael Irvine <michaelirvinedesign@gmail.com>
2020-08-14 14:28:49 -0700

    Building hero-sidebar template

--- src/js/searchToggle.js
+++ src/js/searchToggle.js
@@ -1,4 +1,5 @@
 const searchToggle = () => {
+  const header = document.querySelector('.inner-header__container');
   const openSearchBar = document.querySelector('#open-search-bar');
   const searchBar = document.querySelector('.header-widget-area');
   const menuToggleButton = document.querySelector('.menu-toggle');
@@ -20,6 +21,34 @@ const searchToggle = () => {
     } else {
       menuToggleButton.disabled = false;
       menuToggleButton.setAttribute('aria-disabled', 'false');
+    }
+  });
+
+  document.addEventListener('click', (e) => {
+    const isHeader = header.contains(e.target);
+    const isSearchBar = searchBar.contains(e.target);
+
+    if (
+      !isSearchBar &&
+      !isHeader &&
+      searchBar.classList.contains('is-active')
+    ) {
+      searchBar.classList.remove('is-active');
+
+      // Toggle ARIA Pressed attribute
+      if (openSearchBar.getAttribute('aria-pressed') === 'true') {
+        openSearchBar.setAttribute('aria-pressed', 'false');
+      } else {
+        openSearchBar.setAttribute('aria-pressed', 'true');
+      }
+
+      if (searchBar.classList.contains('is-active')) {
+        menuToggleButton.disabled = true;
+        menuToggleButton.setAttribute('aria-disabled', 'true');
+      } else {
+        menuToggleButton.disabled = false;
+        menuToggleButton.setAttribute('aria-disabled', 'false');
+      }
     }
   });
 };
---
+++ template-hero-sidebar.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Template Name: Hero Sidebar
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package CarbminLab_Theme
+ */
+
+get_header();
+?>
+
+<div class="half-screen-hero__container">
+  <div class="half-screen-hero__image" style="background-image: url(<?php echo the_field('hst-hero-image') ?>)"></div>
+  <div class="half-screen-hero__overlay" style="background-color: <?php echo the_field('hst_hero_overlay_color') ?>; 
+			 				opacity: <?php echo the_field('hst_hero_overlay_opacity'); ?>"></div>
+
+  <div class="half-screen-hero__inner-container">
+    <div class="half-screen-hero__column-one">
+      <h1>
+        <?php echo the_title(); ?>
+      </h1>
+    </div>
+    <div class="half-screen-hero__column-two">
+
+    </div>
+  </div>
+
+</div>
+<main id="primary" class="site-main template__hero-sidebar">
+  <?php if (have_rows('hst_sidebar_menu')) : ?>
+  <aside class="sidebar--jump-links__container">
+    <h4>On This Page:</h4>
+    <ul class="hst_sidebar_menu">
+      <?php while (have_rows('hst_sidebar_menu')) : the_row();
+					$linkTitle = get_sub_field('hst_sidebar_menu_item');
+					$linkUrl = get_sub_field('hst_sidebar_menu_item_link');
+				?>
+      <li>
+        <a href="<?php echo $linkUrl; ?>"><?php echo $linkTitle; ?></a>
+      </li>
+      <?php endwhile; ?>
+    </ul>
+  </aside>
+  <?php endif; ?>
+
+  <section class="hero-sidebar__main-content">
+    <?php
+		while (have_posts()) : ?>
+    <?php the_post();
+			the_content();
+		endwhile; // End of the loop.
+		?>
+  </section>
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
